2c250c347c06c730d0d967cab485818d02ad293e
iamashay <ashayjsl@gmail.com>
2024-07-30 02:56:45 +0530

    fix hydration error in div inside p of statusinfo

--- frontend/src/components/StatusInfo.jsx
+++ frontend/src/components/StatusInfo.jsx
@@ -3,7 +3,7 @@ export default function StatusInfo({ statusDetails, currentStatus }) {
     // console.log(statusDetails)
 
     return (
-        <div className="flex items-center">
+        <span className="flex items-center">
             {statusDetails?.map((statusInfo) => {
                 const { status, text, color } = statusInfo || { text: "No status information available.", color: "bg-gray-500" }
                 if (currentStatus != status) return
@@ -14,7 +14,7 @@ export default function StatusInfo({ statusDetails, currentStatus }) {
                     </>
                 )
             })}
-        </div>
+        </span>
 
 
     );
